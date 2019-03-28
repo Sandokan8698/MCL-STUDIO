9e52ad2609406f3995b8225bc867201cd92da6a6
denysabdel <denys@aktiun.com>
2019-03-27 23:35:43 -0500

    REFACTOR LAYOUT COMPONENT LOCATION

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,7 +7,7 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {UserProfileComponent} from './user-profile/user-profile.component';
 import {TableListComponent} from './table-list/table-list.component';
 import {TypographyComponent} from './typography/typography.component';
-import {IconsComponent} from '../../icons/icons.component';
+import {IconsComponent} from './icons/icons.component';
 import {MapsComponent} from './maps/maps.component';
 import {NotificationsComponent} from './notifications/notifications.component';
 import {UpgradeComponent} from './upgrade/upgrade.component';
@@ -33,6 +33,7 @@ import {ComponentsModule} from '../../components/components.module';
         MatInputModule,
         MatSelectModule,
         MatTooltipModule,
+        ComponentsModule
 
     ],
     declarations: [
